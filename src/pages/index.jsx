86db0361e0c8516819ab878cf5dde267c02a43ec
Wesam <wesamabdelaziz28@gmail.com>
2026-06-30 13:52:52 +0300

    redirect fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  const authorUrl = useBaseUrl('/authors/wesam');
-  return <Redirect to={authorUrl} />;
+  return <Redirect to="/authors/wesam" />;
 }
